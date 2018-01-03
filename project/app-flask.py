6cece0086523307fe0aab445a43e3cee6c1bfbc0
JustChillin21 <JustChillin21@users.noreply.github.com>
2018-01-03 01:05:28 +0100

    Section 10 Lesson 122 - Creating User Profile
    +profile.html
        -created profile html file and passed variables to it using {{}}
        -passed user name and user email
    profile()
        -loads the user using session info incase we don't go via the login
        route.
        -if no user exists, redirects back to the /login/tritter page
    =have to modify the twitter utils file to return from website authentication.
    -should make an auto verification function in console version
    -need to clean up code

--- project/app-flask.py
+++ project/app-flask.py
@@ -1,7 +1,6 @@
 """FLASK"""
 
-from flask import Flask, render_template, session, redirect, request
-
+from flask import Flask, render_template, session, redirect, request, url_for
 from menu import Menu
 from save import create_user
 from twitter_utils import get_request_token, get_oauth_verifier_url, get_access_token
@@ -10,21 +9,7 @@ from user import User
 app = Flask(__name__)
 app.secret_key='d5jQGvNZzoLSmg6eut' #Signing session using secret key #Section 10 Lession 120
 
-# try:
-    # Database.initialize(minconn=1, maxconn=10, user='y2venom', password='jun34u2I',
-    #                     database=input("Database name?"),
-    #                     host='SpiderVault')
-# Database.initialize(minconn=1, maxconn=10, user='y2venom', password='jun34u2I',
-#                         database="learning",
-#                         host='SpiderVault')
-#     cont = True
-#     print("Loading...")
-# except pg.Error:
-#     print("Database Connection Error.")
-#     print "Create a new Database"
-    # print(choice("create a new database"))
-    # self.create_database()
-Menu.display_table_columns()
+Menu.display_table_columns() #initialize database and return column names
 @app.route('/') ## http://127.0.0.1:4995/  #Section 10 Lession 120
 def homepage():
     return render_template('home.html')
@@ -46,12 +31,21 @@ def twitter_auth():
     screen_name=access_token['screen_name']
     token=Menu.param_type(access_token['screen_name'])
     print("loading from user...{}{}".format(screen_name, token))
+
     user=User.load_from_db_by_token(token)
     if not user:
         user=create_user(access_token)
         user.save_to_db()
     session['screen_name']=user.screen_name
-    return user.screen_name + " is loaded"
+    return redirect(url_for('profile'))
+
+@app.route('/profile')
+def profile():
+
+    user=User.load_from_db_by_token((session['screen_name'],'screen_name'))
+    if not user:
+        return redirect(url_for('/login/twitter'))
+    return render_template('profile.html', screen_name=user.screen_name,email=user.email )
 
 
 app.run(port=4995, debug=True)
